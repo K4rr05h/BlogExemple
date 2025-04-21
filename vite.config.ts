1e916cd2c6d4f505a09668d028e735275bf10fbe
K4rr05h <lucenajudymar@gmail.com>
2025-04-21 19:59:31 -0300

    Correção

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/BlogExemple/",
+  base: '/BlogExemple/',
   plugins: [
     react(),
     tailwindcss(),
